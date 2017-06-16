42482eddf4b2226822352766e17fd37da27d4603
DESKTOP-9UTOHN2\chenliangxs <chenliangxs@gmail.com>
2017-06-15 21:46:34 -0700

    NPC exchange
    exchange items with NPC

--- Assets/Scripts/NPC.cs
+++ Assets/Scripts/NPC.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class NPC : MonoBehaviour {
-
+    public GameObject Item_UI_prefab;
 	// Use this for initialization
 	void Start () {
 		
