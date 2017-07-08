20277259573383b331875db88be2de56c91019fd
ZitaoQiu <qzt474838957@gmail.com>
2017-07-08 11:59:59 -0700

    fixed the size of slots in bag panel

--- Assets/Scripts/BagManager.cs
+++ Assets/Scripts/BagManager.cs
@@ -26,8 +26,6 @@ public class BagManager : MonoBehaviour {
             {
                 //Create a slot
                 GameObject slot = Instantiate(slotPrefab) as GameObject;
-                //Set the size of the slot
-                slot.transform.localScale = new Vector3(slotSize / 50.0f, slotSize / 50.0f, 1f);
                 //Set it to be the child of the Bag-Panel
                 slot.transform.SetParent(this.transform);
                 //Name the slot by its position
@@ -37,6 +35,8 @@ public class BagManager : MonoBehaviour {
                     (windowSize.x - windowSize.x / (inventorySize.x) + slotSize) / (inventorySize.x) * (x - 0.5f) + (windowSize.x / (inventorySize.x) - slotSize) / 2,
                     (windowSize.y - windowSize.y / (inventorySize.y) + slotSize) / (inventorySize.y) * (-y + 0.5f) - (windowSize.y / (inventorySize.y) - slotSize) / 2,
                     0);
+                //Set the size of the slot
+                slot.transform.localScale = new Vector3(slotSize / 50.0f, slotSize / 50.0f, 1f);
             }
         }
         this.gameObject.SetActive(false);
